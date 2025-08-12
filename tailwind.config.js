9ede09f32a02b080dc02e7d53ac0e74507282607
Keith <kayceecode@gmail.com>
2025-08-11 21:42:05 -0400

    added to services. and changed bg of events

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,13 @@
 module.exports = {
     theme: {
       extend: {
+        fontFamily: {
+          custom: ['Russo One'], 
+        },
         colors: {
           'snow-white': '#FFFAFA',
           'secondary-green': '#34d399',
+          'deepskyblue': '#00bfff'
         },
       },
     },
