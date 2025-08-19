081dc9f64211d77adc22fe243845cec9ab668205
kaycee <kaycee@minty-razer.mynetworksettings.com>
2025-08-19 16:29:26 -0400

    fixed gsap-animations with nav

--- src/main.js
+++ src/main.js
@@ -3,32 +3,42 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 //import '@fortawesome/fontawesome-free/css/all.min.css';
 import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger'; // If using ScrollTrigger for scroll-based animations
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin'; // If using ScrollTrigger for scroll-based animations
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollToPlugin);
 
-const eventsScroll = document.getElementById('scrollEvents');
-const eventsSection = document.getElementById('events');
-
-const servicesScroll = document.getElementById('scrollServices');
+const servicesScrollBtn = document.getElementById('scrollServices');
 const servicesSection = document.getElementById('services');
 
-const contactsScroll = document.getElementById('scrollContacts');
+const eventsScrollBtn = document.getElementById('scrollEvents');
+const eventsSection = document.getElementById('events');
+
+const contactsScrollBtn = document.getElementById('scrollContacts');
 const contactsSection = document.getElementById('contacts');
 
-eventsScroll.addEventListener('click', () => {
-    // Scroll to the target section
-    gsap.to(window, { duration: 1, scrollTo: { bottom: eventsSection.getBoundingClientRect().bottomp }});
+servicesScrollBtn.addEventListener('click', () => {
+    console.log("Top Position: ", servicesScrollBtn.getBoundingClientRect().top );
+    console.log("Bottom Position: ", servicesScrollBtn.getBoundingClientRect().bottom );
+    
+    gsap.to(window, {
+    duration: 1, // Example: animation duration in seconds
+    scrollTo: '#services'
+    });
 });
 
-servicesScroll.addEventListener('click', () => {
-    // Scroll to the target section
-    gsap.to(window, { duration: 1, scrollTo: servicesSection });
+eventsScrollBtn.addEventListener('click', () => {
+    gsap.to(window, {
+        duration: 1,
+        scrollTo: '#events'
+    });
 });
 
-contactsScroll.addEventListener('click', () => {
-    // Scroll to the target section
-    gsap.to(window, { duration: 1, scrollTo: contactsSection });
+contactsScrollBtn.addEventListener('click', () => {
+    gsap.to(window, 
+    { 
+        duration: 1,
+        scrollTo: '#contacts'
+        });
 });
 
 document.querySelector('#app').innerHTML = ``
