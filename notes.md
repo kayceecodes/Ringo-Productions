d9c47f6244e12059dfe47f2ee6b61d2d3cc94056
Keith <kayceecode@gmail.com>
2025-08-19 12:09:31 -0400

    fixed css load diffs using vite

---
+++ notes.md
@@ -0,0 +1,2 @@
+*August 18, 2025*
+If you want to use things such as z-index you need to set an element to position: relative. Z-index does not work with position: static.
--- src/main.js
+++ src/main.js
@@ -1,25 +1,36 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-import '@fortawesome/fontawesome-free/css/all.min.css';
+//import '@fortawesome/fontawesome-free/css/all.min.css';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger'; // If using ScrollTrigger for scroll-based animations
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+gsap.registerPlugin(ScrollTrigger);
 
-setupCounter(document.querySelector('#counter'))
+const eventsScroll = document.getElementById('scrollEvents');
+const eventsSection = document.getElementById('events');
+
+const servicesScroll = document.getElementById('scrollServices');
+const servicesSection = document.getElementById('services');
+
+const contactsScroll = document.getElementById('scrollContacts');
+const contactsSection = document.getElementById('contacts');
+
+eventsScroll.addEventListener('click', () => {
+    // Scroll to the target section
+    gsap.to(window, { duration: 1, scrollTo: { bottom: eventsSection.getBoundingClientRect().bottomp }});
+});
+
+servicesScroll.addEventListener('click', () => {
+    // Scroll to the target section
+    gsap.to(window, { duration: 1, scrollTo: servicesSection });
+});
+
+contactsScroll.addEventListener('click', () => {
+    // Scroll to the target section
+    gsap.to(window, { duration: 1, scrollTo: contactsSection });
+});
+
+document.querySelector('#app').innerHTML = ``
+
+//setupCounter(document.querySelector('#counter'))
